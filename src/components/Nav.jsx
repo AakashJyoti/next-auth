40801786da31fdb72d27f7d92d3eef7f7f9d79ab
AakashJyoti <akjyoti572@gmail.com>
2023-11-21 23:55:14 +0530

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,9 @@
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 
-export default function Nav() {
+export default async function Nav() {
+  const session = await getServerSession(options);
   return (
     <header className="bg-gray-600 text-white">
       <nav className="flex justify-between items-center w-full px-10 py-4">
@@ -11,6 +14,11 @@ export default function Nav() {
           <Link href={"/clientMember"}>Client Member</Link>
           <Link href={"/member"}>Member Page</Link>
           <Link href={"/public"}>Public</Link>
+          {session ? (
+            <Link href={"/api/auth/signout?callbackUrl=/"}>Logout</Link>
+          ) : (
+            <Link href={"/api/auth/signin"}>Login</Link>
+          )}
         </div>
       </nav>
     </header>
