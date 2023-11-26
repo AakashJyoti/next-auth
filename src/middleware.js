c86f82656c57c4a63db7d11a19a21017c0518433
AakashJyoti <akjyoti572@gmail.com>
2023-11-26 19:40:02 +0530

    update with middleware auth

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,26 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+// export { default } from "next-auth/middleware";
+
+export default withAuth(
+  function middleware(req) {
+    console.log("Middleware", req.nextUrl.pathname);
+    console.log("Middleware", req.nextauth.token.role);
+
+    if (
+      req.nextUrl.pathname.startsWith("/createUser") &&
+      req.nextauth.token.role !== "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/denied", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = {
   matcher: ["/createUser"],
